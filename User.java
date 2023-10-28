f8d5bfff39d3d33d43fa4dfd3d7a9d99335f7d74
Nafsin Nasama Salmee <121750870+NafsinNNS@users.noreply.github.com>
2023-10-28 14:08:44 +0600

    Update User.java

--- User.java
+++ User.java
@@ -1,14 +1,28 @@
 package jobSearchAndApplicationManagementSystem;
-import java.io.*;
-import java.util.*;
-class User {
+
+public class User {
     private String username;
     private String password;
-    // Add other user attributes like fullName, dateOfBirth, etc.
+    private String fullName;
+    private String dateOfBirth;
+    private String institute;
+    private String degree;
+    private String cgpa;
+    private String phoneNumber;
+    private String email;
+    private String address;
 
-    public User(String username, String password) {
+    public User(String username, String password, String fullName, String dateOfBirth, String institute, String degree, String cgpa, String phoneNumber, String email, String address) {
         this.username = username;
         this.password = password;
+        this.fullName = fullName;
+        this.dateOfBirth = dateOfBirth;
+        this.institute = institute;
+        this.degree = degree;
+        this.cgpa = cgpa;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+        this.address = address;
         // Initialize other attributes here.
     }
 
@@ -18,5 +32,37 @@ class User {
 
     public String getPassword() {
         return password;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public String getInstitute() {
+        return institute;
+    }
+
+    public String getDegree() {
+        return degree;
+    }
+
+    public String getCgpa() {
+        return cgpa;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getAddress() {
+        return address;
     }
 }
